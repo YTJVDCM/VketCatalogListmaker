08e5987da88d44a2516669b67f9d577d0fcfbd8f
YTJVDCM <yamaaaaaatai@gmail.com>
2020-03-11 14:07:57 +0900

    インストーラー完成したぜいいぇぇぇぇぇぇぇぇぇぇぇい！！！！

--- launch.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-#include <windows.h>
-using namespace std;
-
-int main(){
-    RegSetValueEx(HKEY_LOCAL_MACHINE,"SOFTWARE\\RegisteredApplications",0,NULL,REG_OPTION_NON_VOLATILE,KEY_ALL_ACCESS,)
-    return 0;
-}
